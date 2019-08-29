eacd337355fa05971dff29f52a3f88b6991832f5
huolongshen <635360172@qq,com>
2019-08-29 21:20:17 +0800

    第一个

--- renxi/functions.js
+++ renxi/functions.js
@@ -58,3 +58,14 @@ function timeElapse(date){
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
+
+function toggleSound() {
+    var music = document.getElementById("bgmMusic");//获取ID  
+    //console.log(music);
+    //console.log(music.paused);
+    if (music.paused) { //判读是否播放  
+         music.paused=false;
+         music.play(); //没有就播放 
+     }  
+            
+ }
